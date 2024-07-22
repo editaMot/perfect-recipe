2efdaf1524de974faf7c11dc3fd2b22607d9877b
Edita Motuze <edita.motuze@gmail.com>
2024-07-22 11:16:41 +0300

    feat: created CategoryCard component

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,35 @@
+import { Box, Typography } from "@mui/material";
+
+type CategoryCardProps = {
+  categoryName: string;
+  categoryImage: string;
+};
+
+const CategoryCard = ({ categoryName, categoryImage }: CategoryCardProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: { xs: 1, sm: 2 },
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: { xs: 100, sm: 120, md: 150 },
+          height: { xs: 100, sm: 120, md: 150 },
+          borderRadius: "50%",
+          backgroundImage: `url(${categoryImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+      <Typography>{categoryName} recipes</Typography>
+    </Box>
+  );
+};
+
+export default CategoryCard;
