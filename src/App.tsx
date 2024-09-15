09c831298dbcfcf2669b430ef807b375d41fc3af
Edita Motuze <edita.motuze@gmail.com>
2024-09-15 21:54:42 +0300

    feat: setup toaster for displaying notifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
+import ToasterConfig from "./configs/ToasterConfig";
 import RoutesConfig from "./routes/RoutesConfig";
 
 const queryClient = new QueryClient();
@@ -15,6 +16,7 @@ function App() {
           <RoutesConfig />
         </BrowserRouter>
       </Suspense>
+      <ToasterConfig />
     </QueryClientProvider>
   );
 }
---
+++ src/configs/ToasterConfig.tsx
@@ -0,0 +1,27 @@
+import { Toaster } from "react-hot-toast";
+
+const ToasterConfig = () => {
+  return (
+    <Toaster
+      position="top-center"
+      gutter={15}
+      containerStyle={{ margin: "8px" }}
+      toastOptions={{
+        success: {
+          duration: 3000,
+        },
+        error: {
+          duration: 3000,
+        },
+        style: {
+          fontSize: "16px",
+          maxWidth: "500px",
+          padding: "16px 24px",
+          backgroundColor: "background.default",
+        },
+      }}
+    />
+  );
+};
+
+export default ToasterConfig;
