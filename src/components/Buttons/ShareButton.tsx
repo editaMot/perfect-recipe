d1eaf1fa2225b0b1976b8bf85119cad7b310c378
Edita Motuze <edita.motuze@gmail.com>
2024-09-15 21:56:37 +0300

    feat: created btn for copying current recipe url

---
+++ src/components/Buttons/ShareButton.tsx
@@ -0,0 +1,33 @@
+import ShareIcon from "@mui/icons-material/Share";
+import { IconButton, Tooltip } from "@mui/material";
+import toast from "react-hot-toast";
+
+interface ShareButtonProps {
+  title: string;
+}
+
+const ShareButton: React.FC<ShareButtonProps> = ({ title }) => {
+  const handleURLCopy = () => {
+    const currentUrl = window.location.href;
+
+    navigator.clipboard
+      .writeText(currentUrl)
+      .then(() => {
+        toast.success("Recipe URL copied to clipboard!");
+      })
+      .catch((error) => {
+        toast.error("Failed to copy the URL.");
+        throw new Error(error);
+      });
+  };
+
+  return (
+    <Tooltip title={title} onClick={handleURLCopy}>
+      <IconButton color="primary">
+        <ShareIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default ShareButton;
