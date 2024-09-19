05ac11930e18072b76eaeaca4f4b67edc609257b
Edita Motuze <edita.motuze@gmail.com>
2024-09-19 21:14:44 +0300

    feat: created section for recipes categories

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import Hero from "../components/Hero/Hero";
 import NewsletterSection from "../components/NewsletterSection";
 import Partners from "../components/Partners";
+import PopularCategoriesSection from "../components/PopularCategoriesSection";
 import RecipeSharingCard from "../components/RecipeSharingCard";
 
-const Home = () => (
+const Home: React.FC = () => (
   <>
     <Hero />
     <RecipeSharingCard />
     <NewsletterSection />
+    <PopularCategoriesSection />
     <Partners />
   </>
 );
