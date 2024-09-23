ebf600b0b792983f4ddb42a5613d00fe1ab29a72
Edita Motuze <edita.motuze@gmail.com>
2024-09-23 15:32:53 +0300

    feat: created blog section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
+import BlogSection from "../components/BlogSection";
 import Hero from "../components/Hero/Hero";
 import NewsletterSection from "../components/NewsletterSection";
 import Partners from "../components/Partners";
-import RecipeSharingCard from "../components/RecipeSharingCard";
+import RecipeSharingCard from "../components/Recipe/RecipeSharingCard";
 
-const Home = () => (
-  <>
-    <Hero />
-    <RecipeSharingCard />
-    <NewsletterSection />
-    <Partners />
-  </>
-);
+const Home: React.FC = () => {
+  return (
+    <>
+      <Hero />
+      <RecipeSharingCard />
+      <BlogSection />
+      <NewsletterSection />
+      <Partners />
+    </>
+  );
+};
 
 export default Home;
